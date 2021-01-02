07809b3a84cbc33a0a18bf490913b033962bf3d2
Brad Reed <shenmusha@yahoo.com>
2021-01-02 11:47:10 -0500

    More primitive types updtates. (chapter 3)

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace HelloWorld
             int num2 = 20;
             int sum = num1 + num2;
 
+            double d1 = 3.5;
+            double d2 = 4.337;
+            double sumD = d1 / d2;
+            
+
             Console.WriteLine("The sum of " + num1 + " and " + num2+" is " +sum);
+            Console.WriteLine(d1 + " divided by "+ d2+ " is " +sumD);
             Console.Read();
         }
     }
