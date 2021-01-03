c2b5dd2675ecaff83ef40f41f80761a72b38df70
Brad Reed <shenmusha@yahoo.com>
2021-01-03 14:41:57 -0500

    PArsing String to Int32

--- Program.cs
+++ Program.cs
@@ -7,31 +7,14 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            //implicit conversion
-            int num = 55179;
-            long bigNumber = num;
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = myString + mySecondString;
 
-            float myFloat = 13.37f;
-            double myNewDouble = myFloat;
-
-            double myDouble = 13.37;
-            int myInt;
-            //Explicit conversion
-            //cast double to int;
-            myInt = (int)myDouble;
-
-            //type conversion
-            string myString = myDouble.ToString();//-"13.37"
-            num.ToString();
-            string myFloatString = myFloat.ToString();
-
-            bool sunIsShining = false;
-
-            string myBoolString = sunIsShining.ToString();
-
-            Console.WriteLine(myBoolString);
-
-            
+            Console.WriteLine(resultInt);
             
         }
     }
