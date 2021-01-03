85bad28afc04cb82c9269321106ac5691bda1610
Brad Reed <shenmusha@yahoo.com>
2021-01-03 14:22:11 -0500

    Casting double-int

--- Program.cs
+++ Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 
 namespace HelloWorld
-{
+{  
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Clear();
-            Console.Write("Enter a string and press enter :");
-            string readInput = Console.ReadLine();
-            Console.WriteLine("You have entered {0}", readInput);
+            double myDouble = 13.37;
+            int myInt;
 
-            Console.Write("Enter a string and press enter :");
-            int asciiValue = Console.Read();
-            Console.WriteLine("ASCII value is {0}:", asciiValue);
-            Console.ReadKey();
+            //cast double to int;
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
             
         }
     }
