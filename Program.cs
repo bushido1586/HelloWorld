40c83e6f9dfa7559df457dc2eab58fc6c8545ba5
Brad Reed <shenmusha@yahoo.com>
2021-01-02 23:37:21 -0500

    Console Color Change

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Enter a string and press enter :");
             string readInput = Console.ReadLine();
             Console.WriteLine("You have entered {0}", readInput);
@@ -14,7 +16,7 @@ namespace HelloWorld
             int asciiValue = Console.Read();
             Console.WriteLine("ASCII value is {0}:", asciiValue);
             Console.ReadKey();
-
+            
         }
     }
 }
