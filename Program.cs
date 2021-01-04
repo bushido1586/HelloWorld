4396c73195a5f0e65c908f5d8d1c51dc7c964f0e
Brad Reed <shenmusha@yahoo.com>
2021-01-03 21:11:28 -0500

    Constants

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 
 namespace HelloWorld
-{  
+{
+    //Constants/Immutable values which cannot be changed for life of program. 
+
     class Program
     {
+        //constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        const string myBirthday = "11.13.1986";
 
         static void Main(string[] args)
         {
-            string myString = "15";
-            string mySecondString = "13";
-            int num1 = Int32.Parse(myString);
-            int num2 = Int32.Parse(mySecondString);
-            int resultInt = num1 + num2;
-            string result = myString + mySecondString;
-
-            Console.WriteLine(resultInt);
-            
+            Console.WriteLine("My birthday is always going to be: {0}", myBirthday);
+        
         }
     }
 }
