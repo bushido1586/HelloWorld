ff8462f1debf4190cf6344a101e83df084a4d018
Brad Reed <shenmusha@yahoo.com>
2021-01-04 21:04:39 -0500

    Method Practice

--- Program.cs
+++ Program.cs
@@ -2,20 +2,25 @@
 
 namespace HelloWorld
 {
-    //Constants/Immutable values which cannot be changed for life of program. 
 
     class Program
     {
-        //constants as fields
-        const double PI = 3.14159265359;
-        const int weeks = 52, months = 12;
-
-        const string myBirthday = "11.13.1986";
-
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("My birthday is always going to be: {0}", myBirthday);
-        
+            WriteSomething();
+            WriteMessage("I am an argument and am called from a method");
+
+        }
+
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteMessage(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
