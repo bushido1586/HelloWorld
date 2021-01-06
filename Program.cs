49db17681be45cc575472289403e97ca4933156e
Brad Reed <shenmusha@yahoo.com>
2021-01-05 20:33:27 -0500

    More Method making

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine(Multiply(5, 7));
+            Console.WriteLine(Division(25, 13));
         }
 
         public static int Multiply(int num1, int num2)
         {
             return num1 * num2;
         }
-
+        public static double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
     }
 }
