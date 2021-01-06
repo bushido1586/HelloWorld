ab794e70091bbb9766366e9474e305ce871b4e45
Brad Reed <shenmusha@yahoo.com>
2021-01-05 20:30:50 -0500

    Challenge: create multiply method (with return)

--- Program.cs
+++ Program.cs
@@ -8,13 +8,12 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(Add(1, 2), Add(3, 4)));
-            Console.WriteLine(Add(15,31));
+            Console.WriteLine(Multiply(5, 7));
         }
 
-        public static int Add(int num1, int num2)
+        public static int Multiply(int num1, int num2)
         {
-            return num1 + num2;
+            return num1 * num2;
         }
 
     }
