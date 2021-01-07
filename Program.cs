9c4fa830da18d507ba299929ee4313e2fec08c7c
Brad Reed <shenmusha@yahoo.com>
2021-01-06 20:47:13 -0500

    try/catch

--- Program.cs
+++ Program.cs
@@ -8,21 +8,21 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Calculate());
-        }
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter correct type next time.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Exception");
+            }
 
-        public static int Calculate()
-        {
-            Console.WriteLine("Please enter the first number");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Please enter the second number");
-            string input2 = Console.ReadLine();
-
-            int num1 = int.Parse(input1);
-            int num2 = int.Parse(input2);
-
-            int result = num1 + num2;
-            return result;
         }
         
     }
