7ef5cf06a5b17e2017f51570b14d4e226ae4a2d3
Brad Reed <shenmusha@yahoo.com>
2021-01-09 11:00:03 -0500

    nested if statments (login checker)

--- Program.cs
+++ Program.cs
@@ -8,31 +8,25 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the temperature today?");
-            string temperature = Console.ReadLine();
-            int numTemp;
-            int number;
-            bool userEntry = int.TryParse(temperature, out number);
-            if(userEntry)
+            Console.WriteLine("Please enter your usernname: ");
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            userName = Console.ReadLine();
+
+            if (isRegistered)
             {
-                numTemp = number;
+                Console.WriteLine("Hi there, regsitered user");
+                if(userName != "")
+                {
+                    Console.WriteLine("Hi there, "+userName);
+                    if (userName.Equals("admin"))
+                    {
+                        Console.WriteLine("Hello Admin");
+                    }
+                }
             }
-            else
-            {
-                numTemp = 0;
-                Console.WriteLine("Value entered in incorect fomrat, temp set as 0");
-            }
-            if (numTemp < 20)
-            {
-                Console.WriteLine("Take a coat");
-            }else if ( numTemp == 20)
-            {
-                Console.WriteLine("Pants and pullover should be ok");
-            }else if (numTemp >= 30)
-            {
-                Console.WriteLine("Its hot today, shorts are good");
-            }
-      
+
         }
         
     }
