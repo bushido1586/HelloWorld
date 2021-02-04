fab451cd73fed5305d6b7f3b589234ba2905e4ca
Brad Reed <shenmusha@yahoo.com>
2021-02-04 17:32:48 -0500

    Calculate volume of Box

---
+++ Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Box
+    {
+        //member variables
+        public int length;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Legnth is {0}, height is {1}, adn width is {2}. so the volume is {3}", length, height, width, volume = length*height*width);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,27 +8,12 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            //create object of class (same as java)/ Instance of Human
-            Human Brad = new Human("Brad", "Reed", "blue", 34);
-            Brad.Introduction();
+            Box box1 = new Box();
+            box1.length = 3;
+            box1.height = 4;
+            box1.width = 5;
 
-            Human kristin = new Human("Kristin", "Kiro", "brown", 33);
-            kristin.Introduction();
-
-            Human basicHuman = new Human();
-            basicHuman.Introduction();
-
-            Human basicHuman2 = new Human("dave", "smith", "green");
-            basicHuman2.Introduction();
-
-            Human natalie = new Human("Natalie");
-            natalie.Introduction();
-
-            Human michael = new Human("Michael", "Miller");
-            michael.Introduction();
-
-            Human frank = new Human("Frank", "Walter", 40);
-            frank.Introduction();
+            box1.DisplayInfo();
         }
         
     }
