e3020e4a79b5f399573f5a83f9dc3792b03674b6
Brad Reed <shenmusha@yahoo.com>
2021-02-03 19:29:26 -0500

    Challenge for OOP constructors lesson-solution.

--- Human.cs
+++ Human.cs
@@ -9,18 +9,22 @@ namespace HelloWorld
         //member variable
         public string firstName;
         public string lastName;
+        public string eyeColor;
+        public int age;
 
         //constrcutor
-        public Human(string firstName, string lastName)
+        public Human(string firstName, string lastName, string eyeColor, int age)
         {
             this.firstName = firstName;
             this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
         } 
 
         //member method
         public void Introduction()
         {
-            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+            Console.WriteLine("Hi, I'm {0} {1}. I have {2} eyes and I'm {3} years old.", firstName, lastName, eyeColor, age);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //create object of class (same as java)/ Instance of Human
-            Human Brad = new Human("Brad", "Reed");
+            Human Brad = new Human("Brad", "Reed", "blue", 34);
             Brad.Introduction();
 
-            Human kristin = new Human("Kristin", "Kiro");
+            Human kristin = new Human("Kristin", "Kiro", "brown", 33);
             kristin.Introduction();           
         }
         
