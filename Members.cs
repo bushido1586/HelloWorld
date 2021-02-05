0956477a4339b9cdcdbe5e0de2742d89761c1487
Brad Reed <shenmusha@yahoo.com>
2021-02-04 19:10:16 -0500

    Members explination

---
+++ Members.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary;
+
+        //member - public field
+        public int age;
+
+        //property=members exposes jobTitle safely
+        public string JobTitle { get; set; } //-or-
+
+        /* public string JobTitle{
+         get{
+        return jobTitle;
+        }
+        set{
+        jobTitle=value;
+        }
+        } */
+
+        // Method -member
+         public void Introduction(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }else
+            {
+                Console.WriteLine("Hi, my name is {0}, and my Job is {1}. I'm {2} years old.", memberName, jobTitle, age);
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is " + salary);
+        }
+
+        // member -constructor
+
+        public Members()
+        {
+            age = 30;
+            memberName = "lucy";
+            salary = 60000;
+            jobTitle = "Developer";
+            Console.WriteLine("Object created");
+        }
+
+        //member finalizer-destructor
+        ~Members()
+        {
+            // cleanup statemens
+            Console.WriteLine("Deconstruction of Members object");
+            Debug.Write("Deconstruction of Members object");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,14 +8,8 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Box box = new Box(3, 4, 5);
-
-            Console.WriteLine("box1 volume is " + box.Volume);
-            box.Width = 10;
-            Console.WriteLine("Box volume is " + box.Volume);
-            box.DisplayInfo();
-
-            Console.WriteLine("Front Surfcae is " + box.FrontSurcae);
+            Members member1 = new Members();
+            member1.Introduction(true);
             
         }
         
