2688e022763bb45f0642aa121558585336d31320
Brad Reed <shenmusha@yahoo.com>
2021-02-05 20:26:11 -0500

    Array practice 1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,22 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Members member1 = new Members();
-            member1.Introduction(true);
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 16;
+            grades[3] = 10;
+            grades[4] = 5;
+
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            string input = Console.ReadLine();
+            //assign value to array at 0 index
+            grades[0] = int.Parse(input);
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
             
         }
         
