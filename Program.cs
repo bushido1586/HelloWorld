b6c9028f9a3ef5fe355b1329a30c0fe4b98fc669
Brad Reed <shenmusha@yahoo.com>
2021-02-05 20:32:40 -0500

    For loop array

--- Program.cs
+++ Program.cs
@@ -8,28 +8,18 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            int[] grades = new int[5];
+            int[] nums = new int[10];
 
-            grades[0] = 20;
-            grades[1] = 15;
-            grades[2] = 16;
-            grades[3] = 10;
-            grades[4] = 5;
+            for(int i = 0; i <10; i++)
+            {
+                nums[i] = i;
+            }
 
+            for(int j =0; j<10; j++)
+            {
+                Console.WriteLine("elements{0} = {1}", j, nums[j]);
+            }
 
-            Console.WriteLine("grades at index 0 : {0}", grades[0]);
-
-            string input = Console.ReadLine();
-            //assign value to array at 0 index
-            grades[0] = int.Parse(input);
-
-            Console.WriteLine("grades at index 0 : {0}", grades[0]);
-            //other intilization options
-            int[] mathGradesClassA = { 20, 13, 12, 8, 8 };
-
-            int[] mathGradesClassB = new int[] { 15, 20, 3, 17, 18, 15 };
-
-            Console.WriteLine("Length of grades for Class A: {0}", mathGradesClassA.Length);
             
         }
         
